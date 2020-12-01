14c552797c73e09446a86762cd9bf263f118f5b3
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-12-01 11:46:44 +0300

    Add test coverage for profile component

---
+++ src/components/Profile/Profile.spec.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Profile from './Profile';
+
+describe('Profile component', () => {
+  it('Should render greeting', () => {
+    const { getByText } = render(<Profile />);
+    expect(getByText(/Hey, User/i)).toBeInTheDocument();
+  })
+
+  it('Should render current date string in "Month Day, Year" format', () => {
+    const { getByText } = render(<Profile />);
+    const compareStr = new Date().toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    });
+    expect(getByText(compareStr)).toBeInTheDocument();
+  });
+})
